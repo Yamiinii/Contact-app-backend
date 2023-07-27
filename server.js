9641ebc0b5057cad9e7be8153fea4eff3c55732d
Yamini <yamini6375@gmail.com>
2023-07-27 19:52:07 +0530

    Intial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
+const dotenv=require("dotenv").config();
+
+connectDb();
+const app=express();
+
+const port=process.env.PORT||5000;
+
+// app.get("/api/contacts",(req,res)=>{
+//     res.send("Get All Contacts")
+// })
+//pass as json
+app.use(express.json());
+app.use("/api/contacts",require("./routes/contactRoutes"));
+app.use("/api/users",require("./routes/userRoutes"));
+app.use(errorHandler)
+
+app.listen(port,()=>
+    {
+        console.log(`Server running on port ${port}`);
+    });
